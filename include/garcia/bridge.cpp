aeeac11b734c3a7aac53eb95b89897ce36411341
andrewrgarcia <andrewrgarcia@ufl.edu>
2022-07-17 13:19:08 -0400

    move all contents funcs to bridge.cpp

--- include/garcia/bridge.cpp
+++ include/garcia/bridge.cpp
@@ -2,19 +2,90 @@
 // Connecting array data between Python and C++ through the use of JSON files
 // Andrew Garcia, PhD
 
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
 #include <iostream>
 #include <iomanip>
 #include <nlohmann/json.hpp>
-#include <fstream>
+#include <fstream> // std::ofstream
 
 using json = nlohmann::json;
 
-int main()
+void jsonify(std::string filename, std::vector<int> vector, std::vector<int> DIMS)
+{
+
+    int N = (std::cbrt(vector.size()) + 0.5);
+    std::vector<std::vector<int>> map_vars;
+    std::vector<int> val_vars;
+
+    int count = 0;
+    for (int j = 0; j < N; j++)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            int item{vector[N * j + i]};
+            if (item) // item must be non-zero
+            {
+                map_vars.push_back({j, i});
+                val_vars.push_back(item);
+                count++;
+            }
+        }
+    }
+
+    std::ofstream ofs(filename, std::ofstream::out);
+
+    ofs << "{\"map\": [";
+    for (int j = 0; j < count; j++)
+    {
+        ofs << "[";
+        for (int i = 0; i < 2; i++)
+            if (i == 1)
+                ofs << map_vars[j][i];
+            else
+                ofs << map_vars[j][i] << ",";
+        if (j == count - 1)
+            ofs << "]";
+        else
+            ofs << "],";
+    }
+    ofs << "]";
+
+    ofs << ",\"value\": [";
+    for (int j = 0; j < count; j++)
+    {
+        // ofs << "[";
+
+        ofs << val_vars[j];
+
+        if (j == count - 1)
+            ofs << "";
+        else
+            ofs << ",";
+    }
+    ofs << "]";
+
+    ofs << ",\"odims\": [";
+
+    for (int i = 0; i < DIMS.size(); i++)
+    {
+        if (i == DIMS.size() - 1)
+            ofs << DIMS[i];
+        else
+            ofs << DIMS[i] << ",";
+    }
+    ofs << "]}";
+
+    ofs.close();
+}
+
+std::vector<int> load(std::string filename)
 {
     // dictionary of keys (DOK)
     std::cout << "DOK:" << std::endl;
 
-    std::ifstream i("sample.json"); // load JSON
+    std::ifstream i(filename); // load JSON
     json deserl;
     i >> deserl;
 
@@ -59,10 +130,5 @@ int main()
         }
     }
 
-    std::cout << "\nvectorized array: " << std::endl;
-    for (int i = 0; i < vector.size(); i++)
-    {
-        std::cout << vector[i] << " ";
-    }
-    std::cout << std::endl;
+    return vector;
 }
